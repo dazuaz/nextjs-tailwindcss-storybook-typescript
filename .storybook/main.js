6900a7049720cfb260745a53fa9fee30284015dd
Daniel Zuloaga <danielzuloaga@gmail.com>
2020-10-27 17:48:41 -0400

    next 10

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,18 +3,18 @@ const path = require("path")
 module.exports = {
   stories: [
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
-    // "../stories/**/*.stories.mdx",
-    // "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
-  // webpackFinal: async (config, { configType }) => {
-  //   config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"]
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.modules = [path.resolve(__dirname, ".."), "node_modules"]
 
-  //   config.resolve.alias = {
-  //     ...config.resolve.alias,
-  //     // "@/Components": path.resolve(__dirname, "../src/components"),
-  //   }
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      // "@/Components": path.resolve(__dirname, "../src/components"),
+    }
 
-  //   return config
-  // },
+    return config
+  },
 }
