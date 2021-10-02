bc6073ba8dda49c4f16481bf8b7c5b871c7c40a3
Daniel Zuloaga <danielzuloaga@gmail.com>
2021-10-02 09:44:45 -0400

    bump

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  features: {
+    postcss: false,
+  },
   // webpackFinal is modified by nextjs/storybook-plugin to include node-modules and nextjs/babel
   typescript: {
     reactDocgen: 'react-docgen',
